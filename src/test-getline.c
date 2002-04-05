295c693cd5e2542d8c3856137411016ada5c2421
jjo <jjo>
2002-04-05 02:39:16 +0000

    Initial revision

---
+++ src/test-getline.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	/* fp = fopen("/etc/motd", "r"); */
+	fp = stdin;
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
+	while ((read = getline(&line, &len, fp)) != -1) {
+		printf("Retrieved line of length %zu :\n", read);
+		printf("%s", line);
+	}
+	if (line)
+		free(line);
+	return EXIT_SUCCESS;
+}
