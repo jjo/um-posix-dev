1ea5491d42a24f308738539f32559c02a6d32c0c
jjo <jjo>
2002-10-11 20:58:22 +0000

    *** empty log message ***

--- clase-20.d-OLD-cluster/cluster-1-pi.c
+++ clase-20.d-OLD-cluster/cluster-1-pi.c
@@ -1,4 +1,4 @@
-/* $Id: cluster-1-pi.c,v 1.2 2002/10/10 20:24:46 jjo Exp $ */
+/* $Id: cluster-1-pi.c,v 1.3 2002/10/11 20:58:22 jjo Exp $ */
 /*
  * Objetivo: Cluster MOSIX: ca'lculo de PI con multiproceso (no aun...)
  * Implementacion:
@@ -31,9 +31,11 @@
  *         |////|////|//./+----+ 
  *         |////|////|////|///.|..
  *      0  +-------------------+------- - -  - 
- *         0                   1
- *
- *
+ *         0 | | | | | | | | | 1
+ *                         <->   tramo
+ *         <-----><-----><----->
+ *          hijo1  hijo2  hijo3
+ *      
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
@@ -54,25 +56,32 @@
 
 #include "pi_lib.h"
 
+/*
+ * 	Tipos de mensaje
+ */
 enum type {
-	VAL_PI,
-	VAL_FLOPS
+	VAL_PI,		/* mando resultado (calc. parcial de PI) */
+	VAL_INFO	/* mando otra info -- no usado aún */
 };
+/*
+ * 	Estructura del mensaje
+ */
 struct mi_msg_t {
-	enum type type;
+	enum type type;	/* tipo de msg */
 	union {
 		pi_double_t d;
 		int i;
-	} val;
+	} data;		/* data del mensaje */
 };
 int child_calc_intervalo(pi_double_t x0, pi_double_t x1, int pasos, int fdout)
 {
+	/* mensaje a retornar por fdout */
 	struct mi_msg_t msg;
-	pi_double_t pi_val_parcial;
-	pi_val_parcial = pi_integra_intervalo(x0, x1, pasos);
+	/* llama a la función de cálculo REAL, aquí tardará un rato */
+	msg.data.d = pi_integra_intervalo(x0, x1, pasos);
 	msg.type = VAL_PI;
-	msg.val.d = pi_val_parcial;
 	write (fdout, &msg, sizeof(msg));
+	close(fdout);
 	return 0;
 }
 int main (int argc, const char *argv[])
@@ -92,6 +101,7 @@ int main (int argc, const char *argv[])
 	}
 	hijos  = atoi(argv[1]);
 	tramos = atoi(argv[2]);
+	/*	Validación de input */
 	if (tramos <10 || tramos > 100E6) {
 		fprintf(stderr, "ERROR: cantidad de tramos=%d incorrecta\n",
 				tramos);
@@ -108,17 +118,28 @@ int main (int argc, const char *argv[])
 				hijos);
 		return 255;
 	}
+	/* 
+	 * pipe por el cual los hijos devuelven el resultado
+	 * al padre (yo) 
+	 */
 	if (pipe(pipefd)<0) {
 		perror("pipe()");
 		return 1;
 	}
+
+	/* x_delta es la porción para cada hijo */
 	x_delta = 1.0/hijos;
+
+	/* ignoro la muerte de mis hijos */
 	signal(SIGCHLD, SIG_IGN);
 	for (i=0; i < hijos; i++) {
 		switch(fork()) {
 			case 0:	/* hijo */
-				close(pipefd[0]);
-				return child_calc_intervalo(x_delta*i, x_delta*(i+1), tramos/hijos, pipefd[1]);
+				close(pipefd[0]); /* hijo NO LEE */
+				return child_calc_intervalo(x_delta*i, 
+						x_delta*(i+1), 
+						tramos/hijos, 
+						pipefd[1]);
 				/* NOT_REACHED */
 			case -1:
 				perror("fork()");
@@ -127,14 +148,20 @@ int main (int argc, const char *argv[])
 				break;
 		}
 	}
-	close(pipefd[1]);
+	close(pipefd[1]); /* padre NO ESCRIBE */
+
+	/*
+	 * leo (y sumo) los resultados que mis hijos me
+	 * mandan vía pipe
+	 */
 	mi_pi = 0.0;
 	while( read(pipefd[0], &msg, sizeof(msg))>0 ) {
 		switch (msg.type) {
-			case VAL_PI: mi_pi += msg.val.d; break;
+			case VAL_PI: mi_pi += msg.data.d; break;
 			default: break;
 		}
 	}
+	
 	fprintf(stderr, "\n");
 	printf("   PI=%36.34Lf\n", PI);
 	printf("mi_pi=%36.34Lf\n", mi_pi);
