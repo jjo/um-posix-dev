cc63e98b145b4536b9f9b37737fc928b6345e068
jjo <jjo>
2003-05-09 21:02:06 +0000

    *** empty log message ***

--- clase-07.d-fork-offset/test-fork-offset.c
+++ clase-07.d-fork-offset/test-fork-offset.c
@@ -7,20 +7,26 @@
 int main(void) 
 {
 	int status;
-	int fd=open(ARCH, O_RDWR|O_CREAT, 0666);
+	int fd=open(ARCH, O_RDWR|O_CREAT|O_TRUNC, 0666);
 	if (fd<0) {
 		perror(ARCH);
 		return 1;
 	}
-	if (fork()==0) {
-		/* hijo */
-		if (lseek(fd, 1024, SEEK_SET)<0) {
-			perror("lseek");
-			return 1;
-		}
-		return 0;
+
+	write(fd, "Comienzo ...", 12);
+
+	switch(fork()) {
+		case -1: perror("fork()");return 255;
+		case 0:
+			/* hijo */
+			if (lseek(fd, 1024, SEEK_SET)<0) {
+				perror("lseek");
+				return 1;
+			}
+			write(fd, "HIJO: ->", 8);
+			return 0;
 	}
 	wait(&status);
-	write(fd, "Donde-stoy?", 4);
+	write(fd, "Donde-stoy?", 11);
 	return 0;
 }
