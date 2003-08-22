e2025e002819f326577d8a63fa0815a1032e7db2
jjo <jjo>
2003-08-22 20:43:47 +0000

    . Renombrado struct_client-> addr_remoto . Uso read en vez de fgets . 3er argumento: port . while(read()) luego while(write()) para mostrar output del server . shutdown() para "se~alizar" el fin de mis writes

--- clase-15.d-sockets2/tcp_ip_client.c
+++ clase-15.d-sockets2/tcp_ip_client.c
@@ -11,13 +11,16 @@
 int main(int argc, char *argv[]) {
 		
 	int sd;
-	struct sockaddr_in struct_client;
-	socklen_t struct_client_len;
+	struct sockaddr_in addr_remoto;
+	socklen_t addr_remoto_len;
 	char buffer[MAX_SIZE];
+	int nread;
+	unsigned short port;
 	
 	/* Verifico el nro de argumentos */
-	if (argc < 2) {
-		puts("Modo de uso: ./tcp_ip_client <nro ip>");
+	if (argc != 3) {
+		fprintf(stderr, "Uso: %s <nro ip> <puerto>\n",
+				argv[0]);
 		return 1;
 	}
 
@@ -28,28 +31,42 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Inicializo y establezco la estructura del cliente */
-	memset(&struct_client, 0, sizeof(struct_client));
-	struct_client.sin_family = AF_INET;
-	struct_client.sin_port = htons(2411);
+	memset(&addr_remoto, 0, sizeof(addr_remoto));
+	addr_remoto.sin_family = AF_INET;
 	
-	if ( !(inet_aton(argv[1], &struct_client.sin_addr))) {
+	/* convierto la dir. IP a sin_addr */
+	if ( !(inet_aton(argv[1], &addr_remoto.sin_addr))) {
 		perror("inet_aton");
 		return 3;
 	}
+	/* 
+	 * convierto el port a numerico y luego la paso de 
+	 * representacion de CPU -> red 
+	 */
+	port = atoi(argv[2]);
+	addr_remoto.sin_port = htons(port);
 	
 	/* Conecta al socket */
-	struct_client_len = sizeof(struct_client);
-	if (connect(sd, (struct sockaddr *)&struct_client, struct_client_len)) {
+	addr_remoto_len = sizeof(addr_remoto);
+	if (connect(sd, (struct sockaddr *)&addr_remoto, addr_remoto_len)) {
 		perror("connect");
 		return 4;
 	}
-	printf("Cliente conectado. !!!\n");
+	fprintf(stderr, "Cliente conectado.\nPara finalizar: <nada><enter>\n");
 	
 	/* Escribe stdin en el socket */
-	while(fgets(buffer, MAX_SIZE, stdin) != NULL) {
-		if (write(sd, buffer, strlen(buffer))<0) {
+	while( (nread=read(STDIN_FILENO, buffer, sizeof buffer)) > 0) {
+		if (nread==1 && buffer[0] == '\n' ) break;
+		if (write(sd, buffer, nread)<0) {
 			perror("write"); return 1;	
 		}
+	}
+
+	/* no me interesa seguir escribiendo ...*/
+	shutdown(sd, SHUT_WR);
+
+	while( (nread=read(sd, buffer, sizeof buffer)) > 0) {
+		write(STDOUT_FILENO, buffer, nread);
 	}
 	
 	close(sd);
@@ -57,3 +74,13 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+/*
+ * $Log: tcp_ip_client.c,v $
+ * Revision 1.2  2003/08/22 20:43:47  jjo
+ * . Renombrado struct_client-> addr_remoto
+ * . Uso read en vez de fgets
+ * . 3er argumento: port
+ * . while(read()) luego while(write()) para mostrar output del server
+ * . shutdown() para "se~alizar" el fin de mis writes
+ *
+ */
