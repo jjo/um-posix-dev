6391df0f0f42eb0a6e6b455b98c79e3bd1791b39
jjo <jjo>
2003-10-03 20:47:42 +0000

    . cosmetic

--- clase-15.d-sockets2/tcp_ip_server.c
+++ clase-15.d-sockets2/tcp_ip_server.c
@@ -11,14 +11,15 @@
 #define MAX_SIZE 4096
 
 int main(int argc, const char *argv[]) {
-	int sd, csd;				/* descriptores */
+	int sd, conn_sd;				/* descriptores */
 	struct sockaddr_in addr_local;		/* donde voy a escuchar */
-	socklen_t addr_local_len;		/* longitud de addr_local */
+	struct sockaddr_in addr_remoto;		/* el que se conecta */
+	socklen_t addr_len;		/* longitud de addr_local */
 	int i;				/* para setsockopt */
 	pid_t hijo; 				/* pid del hijo */
-	int status, ret;
 	char buffer[MAX_SIZE];			/* Buffer para la lectura */
 	unsigned short port;
+	int nread;
 	
 	/* Crea el socket */	/* Verifico el nro de argumentos */
 	if (argc != 2) {
@@ -49,9 +50,9 @@ int main(int argc, const char *argv[]) {
 	addr_local.sin_port = htons(port);
 		
 	/* Liga el socket a una direccion */
-	addr_local_len = sizeof(addr_local);
+	addr_len = sizeof(addr_local);
 	if ( (bind(sd, (struct sockaddr *)&addr_local, 
-		addr_local_len)) < 0 ) {
+		addr_len)) < 0 ) {
 		perror("bind");
 		return 2;		
 	}
@@ -67,31 +68,30 @@ int main(int argc, const char *argv[]) {
 	printf("\tDireccion IP: %s\n", inet_ntoa(addr_local.sin_addr));
 	
 	/* Ejecuta indefinidamente la aceptacion de conexiones */
-	while ((csd = accept(sd, (struct sockaddr *)&addr_local, 
-		&addr_local_len))>=0) {
+	while ((conn_sd = accept(sd, (struct sockaddr *)&addr_remoto, 
+		&addr_len))>=0) {
 		
 		switch(hijo=fork()) {
 		case -1: /* Hubo un problema con la creacion del hijo */ 
 			break;
 		case 0: /* Codigo del hijo, futuros ZOMBIES ... */
 
-			/* Carga la estructura con todos ceros */
-			memset(buffer, 0, MAX_SIZE);			
+			close(sd);
 			/* Leo una sola vez del socket (supongo suficiente) */
-			while (read(csd, buffer, MAX_SIZE)>0) {
-				write(1, buffer, strlen(buffer)); //->-> stdout 
-				memset(buffer, 0, MAX_SIZE);			
+			while ((nread=read(conn_sd, buffer, MAX_SIZE))>0) {
+				write(STDOUT_FILENO, buffer, nread); 
 			}
-			return 0; /* Muestra q hubo un error en el hijo */
+			return 0;
 			break;
 			
 		default: /* Codigo del padre */			
-			if ((ret = waitpid(0, &status, WNOHANG))<0) {
-				perror("wait");
-			}
+			close(conn_sd);
+			printf("\nNueva conexion pid=%i IP remoto=%s, puerto remoto=%d\n", 
+					hijo,
+					inet_ntoa(addr_remoto.sin_addr),
+					ntohs(addr_remoto.sin_port));
 			break;
 		}
-		printf("\nNueva conexion %i\n", hijo);
 	}
 	close(sd);
 	return 0;
@@ -99,6 +99,9 @@ int main(int argc, const char *argv[]) {
 
 /*
  * $Log: tcp_ip_server.c,v $
+ * Revision 1.3  2003/10/03 20:47:42  jjo
+ * . cosmetic
+ *
  * Revision 1.2  2003/08/22 20:51:48  jjo
  * . Renombrado struct_server -> addr_local
  * . argv[1]: port
