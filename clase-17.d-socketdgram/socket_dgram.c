49bf0b26774907d826da0a4d4d2a3020d3570ff3
jjo <jjo>
2003-10-03 20:41:17 +0000

    . Un solo sockfd

--- clase-17.d-socketdgram/socket_dgram.c
+++ clase-17.d-socketdgram/socket_dgram.c
@@ -14,7 +14,7 @@
 #define GROUP "239.1.2.3"	/* desde 224.0.0.0 hasta 239.255.255.255 */
 int main(void)
 {
-     int sockr, sockw;
+     int sockfd;
      struct sockaddr_in addr_r, addr_w;
      int addrlen_r, addrlen_w;
      struct ip_mreq imr;
@@ -25,8 +25,7 @@ int main(void)
      int nselect;
      int opt;
 
-     ERRSYS(sockr = socket(PF_INET, SOCK_DGRAM, 0));
-     ERRSYS(sockw = socket(PF_INET, SOCK_DGRAM, 0));
+     ERRSYS(sockfd = socket(PF_INET, SOCK_DGRAM, 0));
 
      memset(&addr_r, 0, sizeof(addr_r));
      addr_r.sin_family = AF_INET;
@@ -42,34 +41,37 @@ int main(void)
      imr.imr_multiaddr.s_addr = inet_addr(GROUP);
      imr.imr_interface.s_addr = htonl(INADDR_ANY);
 
-     ERRSYS(setsockopt(sockr, IPPROTO_IP, IP_ADD_MEMBERSHIP, 
+     ERRSYS(setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, 
 			     (void *) &imr, sizeof(struct ip_mreq)));
      opt = 1;
-     ERRSYS(setsockopt(sockr, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)));
-     ERRSYS(bind(sockr, (struct sockaddr *)&addr_r, sizeof(addr_r)));
+     ERRSYS(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)));
+     ERRSYS(bind(sockfd, (struct sockaddr *)&addr_r, sizeof(addr_r)));
      addrlen_r = sizeof(addr_r);
      addrlen_w = sizeof(addr_w);
      while (1) {
 	     FD_ZERO(&rfds);
 	     FD_SET(STDIN_FILENO, &rfds);
-	     FD_SET(sockr,        &rfds);
+	     FD_SET(sockfd,       &rfds);
 
 	     ERRSYS( nselect=select(1024, &rfds, NULL, NULL, NULL) );
 	     if (FD_ISSET(STDIN_FILENO, &rfds)) {
 		     nbuf=snprintf(buf, sizeof(buf), "%s dijo: ", getlogin());
 		     ERRSYS( cnt=read(STDIN_FILENO, buf+nbuf, sizeof(buf)-nbuf) );
 		     if (cnt==0) { fprintf(stderr, "stdin: EOF\n"); break; }
+		     if (buf[nbuf]=='\n') continue;
 		     fprintf(stderr, "** stdin -> red\n");
 
-		     ERRSYS( cnt=sendto(sockw, 
+		     ERRSYS( cnt=sendto(sockfd, 
 				     buf, nbuf+cnt, 0, 
 				     (struct sockaddr *)&addr_w, addrlen_w));
 	     }
-	     if (FD_ISSET(sockr, &rfds)) {
-		     ERRSYS( cnt = recvfrom(sockr, buf, sizeof(buf), 0, 
+	     if (FD_ISSET(sockfd, &rfds)) {
+		     ERRSYS( cnt = recvfrom(sockfd, buf, sizeof(buf), 0, 
 				     (struct sockaddr *)&addr_r, &addrlen_r));
 		     if (cnt==0) { fprintf(stderr, "red: EOF\n"); break; }
-		     fprintf(stderr, "** red -> stdout\n");
+		     fprintf(stderr, "** red (%s:%d)-> stdout\n",
+				     inet_ntoa(addr_r.sin_addr),
+				     ntohs(addr_r.sin_port));
 		     ERRSYS( write(STDOUT_FILENO, buf, cnt) );
 	     }
 		
