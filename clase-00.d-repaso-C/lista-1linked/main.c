186bfc6694ca85e31b1c3e72833ccd6f2b019358
jjo <jjo>
2004-03-19 20:19:32 +0000

    *** empty log message ***

--- clase-00.d-repaso-C/lista-1linked/main.c
+++ clase-00.d-repaso-C/lista-1linked/main.c
@@ -17,11 +17,14 @@ int main(void)
 	struct lista *mi_lista;
 	struct elem *e;
 	char **strp;
+	/* creo una nueva lista */
 	mi_lista=lista_new();
+	/* creo un elemento por palabra y lo appendo a lista */
 	for(strp=palabras;*strp;strp++) {
 		e=elem_new_string(*strp);
 		lista_append_elem(mi_lista, e);
 	}
+	/* para cada elemento de la lista, muestro e->data */
 	LISTA_FOR_EACH(mi_lista, e) {
 		puts(e->data);
 	}
