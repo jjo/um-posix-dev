bb0b0c987655485ce6dc5eecd1f440054e439940
jjo <jjo>
2004-10-15 19:19:02 +0000

    . limpiezas varias ...

---
+++ clase-20.d-threads_condvars/hilo-prodcons.c
@@ -0,0 +1,80 @@
+/* 
+ * 1 producer: genera mensajes que se encolan en lalista (contienen un nro entero)
+ * n consumers: sacan mensajes de lalista y lo procesan (muestra el nro contenido)
+ *
+ * el programa tiene una HORRIBLE race-condition...
+ * ... porque?  `8)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
+#include "lista.h"
+struct lista lalista;
+void parse_args_or_die(int argc, const char *argv[], int *n_mensajes, int *n_consumers);
+void *consumer(void *);
+void *producer(void *);
+
+int main(int argc, const char *argv[])
+{
+	unsigned n_consumers, n_mensajes, i;
+	pthread_t tid_cons, tid_prod;
+
+	parse_args_or_die(argc, argv, &n_mensajes, &n_consumers);
+	lista_init(&lalista);
+
+	for(i=1;i<=n_consumers;i++)
+		pthread_create(&tid_cons, NULL, consumer, (void*)i);
+
+	pthread_create(&tid_prod, NULL, producer, (void*)n_mensajes);
+	pthread_join(tid_prod, NULL);
+
+	lista_destroy(&lalista);
+	printf("Saliendo...\n");
+	return 0;
+}
+
+void *producer(void *arg) {
+	struct mensaje *m;
+	int i, n_mensajes=(int)arg;
+	for (i=0;i<n_mensajes;i++) {
+		printf("producer: i=%d\n", i);
+		m=malloc(sizeof *m);
+		mensaje_init(m);
+		m->num=i;
+		lista_put(&lalista, m);
+	}
+	return NULL;
+}
+void *consumer(void *arg) {
+	unsigned hilo_num=(unsigned)arg;
+	struct mensaje *m;
+	char buf[64]; int cant;
+
+	pthread_detach(pthread_self());
+	while(1) {
+		usleep(10);
+		m=lista_get(&lalista);
+		if (!m) break;
+		cant=snprintf(buf, sizeof buf -1, "                consumer[%02u] num=%d\n", hilo_num, m->num);
+		
+		write(STDOUT_FILENO, buf, cant);
+		free(m);
+	}
+	return NULL;
+}
+void parse_args_or_die(int argc, const char *argv[], int *n_mensajes, int *n_consumers)
+{
+	if (argc!=3) {
+		fprintf(stderr, "uso: %s n_mensajes n_consumers\n"
+				" ej: %s 20 1\n"
+				" ej: %s 20 5\n"
+				, argv[0], argv[0], argv[0]);
+		exit(255);
+	}
+	*n_mensajes=atoi(argv[1]);
+	*n_consumers=atoi(argv[2]);
+	if (!*n_mensajes|| *n_mensajes > 10000 || !*n_consumers || *n_consumers > 100) exit(255);
+}
---
+++ clase-20.d-threads_condvars/lista-sem.c
@@ -0,0 +1,36 @@
+#include <glib.h>
+#include "lista.h"
+#include <assert.h>
+void mensaje_init(struct mensaje *m) 
+{
+	m->num=0;
+}
+void lista_init(struct lista *lista)
+{
+	pthread_mutex_init(&lista->l_lock, NULL);
+	sem_init(&lista->u.l_sem, 0, 0);
+	lista->l_queue=g_queue_new();
+}
+void lista_destroy(struct lista *lista)
+{
+	sem_destroy(&lista->u.l_sem);
+	g_queue_free(lista->l_queue);
+}
+void lista_put(struct lista *lista, struct mensaje *m)
+{
+	pthread_mutex_lock(&lista->l_lock);
+	g_queue_push_tail(lista->l_queue, m);	/* agrego elem a la cola de la lista */
+	pthread_mutex_unlock(&lista->l_lock);
+	sem_post(&lista->u.l_sem);		/* semaforo: UP */
+}
+struct mensaje *lista_get(struct lista *lista)
+{
+	struct mensaje *m;
+	sem_wait(&lista->u.l_sem);		/* semaforo: DOWN */
+	pthread_mutex_lock(&lista->l_lock);
+	m=g_queue_pop_head(lista->l_queue);
+	pthread_mutex_unlock(&lista->l_lock);
+	assert(m!=NULL);
+	return m;	
+}
+
