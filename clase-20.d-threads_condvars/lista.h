2437a9c521f190355758fda0602e7790b709229d
jjo <jjo>
2004-10-15 19:13:08 +0000

    . limpiezas varias ...

--- clase-20.d-threads_condvars/lista.h
+++ clase-20.d-threads_condvars/lista.h
@@ -1,19 +1,17 @@
+#include <glib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include "linux_list.h"
 /* lista 2blemente linkeada */
 struct lista {
-	struct list_head l_head;
-	pthread_mutex_t l_lock;
+	GQueue *l_queue;	/* cola de elementos */
+	pthread_mutex_t l_lock;	/* protege modif de la lista */
 	union {
-		sem_t l_sem;
+		sem_t l_sem;		/* lleva la "cuenta" de cant. de elementos */
 		pthread_cond_t l_cond;
 	} u;
 };
-/* cada mensaje va a ser 1elem. de la lista */
 struct mensaje {
 	int num;
-	struct list_head l_ptr;
 };
 
 
@@ -21,5 +19,7 @@ struct mensaje {
 void lista_init(struct lista *lista);
 void lista_destroy(struct lista *lista);
 void lista_put(struct lista *lista, struct mensaje *m);
-struct mensaje *lista_get(struct lista *lista);
+/** debe DORMIR hasta devolver un elem */
+struct mensaje *lista_get(struct lista *lista); 
+/** inicializa un mensaje (debe estar creado previamente) */
 void mensaje_init(struct mensaje *m);
