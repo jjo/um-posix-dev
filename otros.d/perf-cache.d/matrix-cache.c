bdbc41a864f77f4b6d61dff089f1fae0e8dbded5
jjo <jjo>
2005-08-04 15:05:23 +0000

    *** empty log message ***

---
+++ otros.d/perf-cache.d/matrix-cache.c
@@ -0,0 +1,23 @@
+/* $Id */
+/* Muestra la diferencia de velocidad entre loop externo por filas o columnas,
+ * causada por un buen/mal uso de cache
+ */
+#define COLS 10000
+#define ROWS 1000
+int a[ROWS][COLS];
+int b[ROWS][COLS];
+int c[ROWS];
+int main(void) {
+	int i,j;
+#if   defined( CACHE_BAD )
+	for (j = 0; j < COLS; j++)
+		for (i = 0; i < ROWS; i++)
+#elif defined( CACHE_GOOD )
+    	for (i = 0; i < ROWS; i++)
+		for (j = 0; j < COLS; j++)
+#else
+#error "Por favor compilar con -DCACHE_BAD o -DCACHE_GOOD" 
+#endif
+		    a[i][j] = b[i][j] + c[i];
+	return 0;
+}
